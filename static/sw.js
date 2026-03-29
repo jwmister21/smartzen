35aa2a81a6bc5bb79b599615dda6d0fcbbeed028
jwmister21 <100645292+jwmister21@users.noreply.github.com>
2026-03-29 20:57:53 -0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,23 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "smartzen-cache-v2";
+
+self.addEventListener("install", event => {
   console.log("Service Worker instalado");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/static/icon-192.png",
+        "/static/icon-512.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
